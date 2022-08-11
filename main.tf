dbed38d16b84793330a273c1bf939faa96d995f9
xgro <zero1401@naver.com>
2022-08-11 11:11:31 +0900

    test: gitops terraform

--- main.tf
+++ main.tf
@@ -8,22 +8,15 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "xgro-test-tfstate"
-    key    = "terraform.tfstate"
-    region = "ap-northeast-2"
-    # dynamodb_table  = aws_dynamodb_table.terraform_state_lock.id
+    bucket         = "xgro-test-tfstate"
+    key            = "terraform.tfstate"
+    region         = "ap-northeast-2"
     dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 
   required_version = ">= 1.0.11"
 }
-
-# AWS provider configuration
-# provider "aws" {
-#   profile = "aws"
-#   region  = "ap-northeast-2"
-# }
 
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
--- vpc.tf
+++ vpc.tf
@@ -1,22 +1,15 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "final_mon2-vpc"
+  name = "final_mon3-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-northeast-2a", "ap-northeast-2b"]
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
   public_subnets  = ["10.0.1.0/24"]
 
-  # enable_nat_gateway     = true
-  # single_nat_gateway     = true
-  # one_nat_gateway_per_az = false
-
+  enable_nat_gateway = false
   enable_vpn_gateway = false
-
-  # enable_dns_support   = true
-  # enable_dns_hostnames = true
-
   tags = {
     Terraform   = "true"
     Environment = "dev"
